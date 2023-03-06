b0f9a7d666f6057c504ca0d46c46d51de5806216
Javokhirkhon <javokhirkhon_sh@icloud.com>
2023-03-06 15:45:36 +0000

    project created & logig page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TEAM 5 - AWS</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <style>
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
